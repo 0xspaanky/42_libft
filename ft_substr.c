38e9584a549e757823ebd5432117adde7a0ac8e6
spaaaaaaky <116687257+spaaaaaaky@users.noreply.github.com>
2022-10-29 17:48:47 +0100

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smounafi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: smounafi <smounafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:35:22 by smounafi          #+#    #+#             */
-/*   Updated: 2022/10/18 18:24:17 by smounafi         ###   ########.fr       */
+/*   Updated: 2022/10/29 17:33:24 by smounafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	size_t	j;
 	char	*p;
 
 	if (!s)
 		return (0);
 	if (start >= ft_strlen(s))
 		return (ft_calloc(1, 1));
-	j = start;
-	while (s[j] && j < len)
-		j++;
-	p = (char *)malloc(j + 1);
+	if(len > ft_strlen(s))
+		len = ft_strlen(s);
+	p = (char *)malloc(len + 1);
 	if (!p)
 		return (NULL);
 	i = 0;
